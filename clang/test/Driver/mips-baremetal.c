e1b9a194d4144a61f320b3b1bd11bb59dc4cbbbb
Petr Vesely <petr.vesely@codasip.com>
2025-09-29 11:57:38 +0200

    fixup! [Driver,BareMetal] Replace -lclang_rt.builtins{,-}.a with an absolute path (#82424)

--- clang/test/Driver/mips-baremetal.c
+++ clang/test/Driver/mips-baremetal.c
@@ -23,5 +23,5 @@
 // CHERIABI-SAME: "-L{{.+}}/lib/clang-runtimes/mips64-none-elf/lib"
 // CXX-SAME: "-lc++" "-lc++abi" "-lunwind"
 // CHECK-SAME: "-lc" "-lm"
-// CHECK-SAME: "-lclang_rt.builtins-mips64"
+// CHECK-SAME: "{{.+}}libclang_rt.builtins-mips64.a"
 // CHECK-SAME: "-o" "{{.+}}/mips-baremetal.c.tmp"
